fc1ef80e12586fa55c75c439ed57e117c790d947
fajarulung <109120383+fajarulung@users.noreply.github.com>
2022-09-27 14:22:48 +0700

    Add files via upload

---
+++ php perulangan.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <title>Loop While</title>
+
+        <body>
+            <?php
+            for ($i = 100; $i < 1000; $i++) {
+                echo $i;
+            }
+            ?>
+
+
---
+++ php_html_php.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Demo</title>
+
+</head>
+
+<body>
+    <?php
+    echo 'Kode PHP di sini';
+    //...
+    ?>
+
+    <p>Dokumen HTML</p>
+
+    <?php
+    echo 'Kode PHP di sini';
+    //...
+    ?>
+
+    <?php
+    // deklarasi dan inisialisai
+    $bil = 3;
+    echo  $bil;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       $bil;
+    ?>
+
+    <?php
+    // deklarasi if
+    $a = 5;
+    $b = 10;
+
+    if ($a > $b) {
+        echo 'a lebih besar dari b';
+    }
+    ?>
+
+
+</body>
+</html>
